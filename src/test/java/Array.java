a255663e677a758ce6766ce35199525244a7bba3
rukes <rukescz430@gmail.com>
2018-04-25 10:33:15 +0200

    práce s polem

---
+++ src/test/java/Array.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+public class Array {
+
+    public static void main(String[] args){
+
+        int[] array = new int[10];
+
+        Random random = new Random();
+        for(int i = 0; i < array.length; i++){
+            array[i] = random.nextInt(100) + 1;
+        }
+
+        int min = Integer.MAX_VALUE;
+        int minPos = -1;
+        int max = -1;
+        int maxPos = -1;
+        int count = 0;
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = 0; i < array.length; i++){
+            int number = array[i];
+            if(number < min){
+                min = number;
+                minPos = i;
+            }
+            if(number > max){
+                max = number;
+                maxPos = i;
+            }
+            count += number;
+            builder.append(number).append(", ");
+        }
+        double average = (double) count / (double) array.length;
+
+        System.out.println("Array: " + builder.toString().substring(0, builder.length() - 2));
+        System.out.println("Součet: " + count);
+        System.out.println("Průměr: " + average);
+        System.out.println("Nejnižší hodnota: " + min + " (" + minPos + ")");
+        System.out.println("Nejvyšší hodnota: " + max + " (" + maxPos + ")");
+    }
+}
